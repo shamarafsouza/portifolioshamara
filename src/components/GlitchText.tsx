5272afdee7fc147a4328b4cc8fc53c18f4188eed
SHAMARA FERREIRA DE SOUZA <129136625+shamarafsouza@users.noreply.github.com>
2026-08-10 08:07:22 -0300

    Delete src/components/GlitchText.tsx
    nao será usada

--- src/components/GlitchText.tsx
+++
@@ -1,3 +0,0 @@
-export default function GlitchText({ text, className = '' }: { text: string; className?: string }) {
-  return <span className={`glitch ${className}`} data-text={text}>{text}</span>
-}
