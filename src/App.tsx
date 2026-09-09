73b3f95d9ad206b0a5ce6ea3031d50f550576213
SHAMARA FERREIRA DE SOUZA <129136625+shamarafsouza@users.noreply.github.com>
2026-09-09 12:12:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -342,23 +342,33 @@ export default function App() {
                     {t.projects[i].text}
                   </p>
 
-                  {project.demo && (
-                    <div className="project-video-frame">
-                        <img
-                          src={project.demo}
-                          alt={`Demonstração do projeto ${project.name}`}
-                          className="project-demo"
-                        />
+              {project.demo && (
+                <div className="project-video-frame">
+                {project.demo.endsWith('.mp4') ? (
+                  <video
+                    src={project.demo}
+                    className="project-demo"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                  />
+                  ) : (
+                <img
+                  src={project.demo}
+                  alt={`Demonstração do projeto ${project.name}`}
+                  className="project-demo"
+                />
+              )}
 
-                        <img
-                          src="/molduravideos.png"
-                          alt=""
-                          className="project-frame-overlay"
-                          aria-hidden="true"
-                        />
-                      </div>
-                  )}
-
+    <img
+      src="/molduravideos.png"
+      alt=""
+      className="project-frame-overlay"
+      aria-hidden="true"
+    />
+  </div>
+)}      
                   <div className="tags">
                     {project.tags.map(tag => (
                       <span key={tag}>
